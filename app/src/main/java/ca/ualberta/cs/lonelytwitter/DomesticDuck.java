222b17fb13730cf9febef02b5b1bb49fe0474336
Aman Anand <aanand@ualberta.ca>
2018-09-20 18:55:06 -0600

    DOne

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/DomesticDuck.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.util.Log;
+
+public class DomesticDuck extends Animal implements FlyingBehaviour{
+    public DomesticDuck(String type,String food){
+        super(type,food);
+    }
+    public void swim(){
+        Log.d("cmput-301","Yeah I love to swim");
+    }
+
+    public void doesLikeFlying(){
+        Log.d("Anand","No I fly only when a cat attacks me");
+    }
+}
