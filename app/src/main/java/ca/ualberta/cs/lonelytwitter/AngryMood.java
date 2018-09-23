6c9fd27d04afd0c22108f40fbcac2f4ed50c8e2a
Aman Anand <aanand@ualberta.ca>
2018-09-22 20:33:43 -0600

    Lab1 finished

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/AngryMood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/AngryMood.java
@@ -1,6 +1,15 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
+
 public class AngryMood extends CurrentMood {
+    public AngryMood(){
+        super();
+    }
+    public AngryMood(Date date){
+        super(date);
+    }
+
     @Override
     public String WhichMood(){
         return "Angry";
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/HappyMood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/HappyMood.java
@@ -1,6 +1,15 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
+
 public class HappyMood extends CurrentMood {
+    public HappyMood(){
+        super();
+    }
+    public HappyMood(Date date){
+        super(date);
+    }
+
     @Override
     public String WhichMood(){
         return "Happy";
